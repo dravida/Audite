d79d2d1b40a8378e286350bb842a28a068c43f93
Eddie Olson <eddie.olson@liferay.com>
2014-12-19 16:37:56 -0800

    add parse cloud code

---
+++ Parse/cloud/main.js
@@ -0,0 +1,14 @@
+Parse.Cloud.define("save", function(request, response) {
+  var Text = Parse.Object.extend("Text");
+  var text = new Text();
+  var fromId = request.params.session.from.id;
+  text.set("fromId", fromId);
+  var toId = request.params.session.to.id;
+  text.set("toId", toId);
+  var message = request.params.session.initialText;
+  text.set("message", message);
+  var requestBody = request.body;
+  text.set("raw", requestBody);
+  text.set("visible", false);
+  text.save();
+});
